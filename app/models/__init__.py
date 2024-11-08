45154a60da838a065acb27460fb4eff9f0cc73b2
145921 <serge.kambale@strathmore.edu>
2024-11-08 15:55:32 +0300

    Implements Models Module

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User as User
+from .recruiter import Recruiter as Recruiter
+from .applicant import Applicant as Applicant
+from .education import Education as Education
+from .experience import Experience as Experience
+from .application import Application as Application
+from .organization import Organization as Organization
